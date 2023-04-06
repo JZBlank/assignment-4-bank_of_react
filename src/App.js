bcbe0477dabc648be061cf3fedddcd91d9704398
joyce <joycezhang2065@gmail.com>
2023-04-05 20:01:19 -0400

    setup app + display API information on pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
 import Credits from './components/Credits';
 import Debits from './components/Debits';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {  // Create and initialize state
@@ -28,13 +29,62 @@ class App extends Component {
     };
   }
 
+  // Update state's credit based on user input of new credits
+  addCredit = () => {
+
+    const newCredit = {...this.state.creditList};
+    newCredit.push( {
+      "id": 6,
+      "description": "Tutoring",
+      "amount": 200.78,
+      "date": "2003-09-17T15:58:49Z"
+    });
+
+    this.setState({creditList: newCredit});
+  }
+
+  // Update state's debit based on user input of new debits
+  addDebit = (debitInput) => {
+  }
+
+  // Lifecycle Method componentDidMount() which includes the API requests using given endpoints 
+  async componentDidMount() {
+
+    let linkToCreditAPI = 'https://johnnylaicode.github.io/api/credits.json';
+    let linkToDebitAPI = 'https://johnnylaicode.github.io/api/debits.json';
+
+    // Await for promise (completion) returned from API calls
+    try {
+      let creditResponse = await axios.get(linkToCreditAPI);
+      let debitResponse = await axios.get(linkToDebitAPI);
+
+      console.log(creditResponse); // Prints out credit Response
+      console.log(debitResponse); // Prints out debit Response
+
+      // To get data object in the response, use "response.data"
+      this.setState({creditList: creditResponse.data});
+      this.setState({debitList: debitResponse.data});
+
+    }
+    catch(error) { // Print out errors on console when tehre is an error response
+      if (error.response){
+        // The request was made, and server responded with error message and status code.
+        console.log(error.creditResponse.data); // Print out error message(e.g., Not Found)
+        console.log(error.creditResponse.status); // Print out error status code (e.g., 404)
+      
+        console.log(error.debitResponse.data); // Print out error message(e.g., Not Found)
+        console.log(error.debitResponse.status); // Print out error status code (e.g., 404)
+      }
+    }
+  }
+
   // Update state's currentUser (userName) after "Log In" button is clicked
   mockLogIn = (logInInfo) => {  
     const newUser = {...this.state.currentUser};
     newUser.userName = logInInfo.userName;
     this.setState({currentUser: newUser})
   }
-
+  
   // Create Routes and React elements to be rendered using React components
   render() {  
     // Create React elements and pass input props to components
@@ -43,13 +93,14 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     )
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.creditList} />) 
-    const DebitsComponent = () => (<Debits debits={this.state.debitList} />) 
+    const CreditsComponent = () => (<Credits credits={this.state.creditList} accountBalance={this.state.accountBalance} addCredit={this.addCredit} />) 
+    const DebitsComponent = () => (<Debits debits={this.state.debitList} accountBalance={this.state.accountBalance} addDebit={this.addDebit}   />) 
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-example-code-gh-pages">
+      <Router basename="/assignment-4-bank_of_react">
         <div>
+          
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/login" render={LogInComponent}/>
--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,16 +4,48 @@ src/components/Credits.js
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
+import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 
 const Credits = (props) => {
+  
+  // Create the list of Credit items
+  let creditsView = () => {
+    const { credits } = props;
+    return credits.map((credit) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+      let date = credit.date.slice(0,10); 
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    });
+  }
+
+  // When user clicks Add Credit button, store user data and update current balance
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addCredit(this.state.creditList);  // Update state in the top-level component (App.js)
+  }
+  
+
+  // Render the list of Debit items and a form to input new Debit item
   return (
     <div>
       <h1>Credits</h1>
-      <br/>
+
+      {creditsView()}
+      <br/><br/>
+
+      <form onSubmit={handleSubmit}>
+        <label>Description</label>
+        <input type="text" name="description" />
+        <label>Amount</label>
+        <input type="number" name="amount" />
+        <button type="submit" >Add Credit</button>
+      </form>
+
+      <br/><br/>
+      <AccountBalance accountBalance={props.accountBalance}/>
+      <br></br>
       <Link to="/">Return to Home</Link>
     </div>
   );
 }
-
 export default Credits;
